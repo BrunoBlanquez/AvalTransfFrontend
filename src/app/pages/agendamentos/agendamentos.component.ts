8f826603191214733fd31e3780980237967cb64d
Bruno Blanquez <b.blanquez@yahoo.com.br>
2025-05-15 17:40:51 -0300

    Criando componentes e conexão com backend

--- src/app/pages/agendamentos/agendamentos.component.ts
+++ src/app/pages/agendamentos/agendamentos.component.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Transferencia } from '../../models/transferencia';
+import { TransferenciasService } from '../../services/transferencias.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-agendamentos',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './agendamentos.component.html',
   styleUrl: './agendamentos.component.css'
 })
 export class AgendamentosComponent {
+  novaTransferencia: Transferencia = {
+    contaOrigem: '',
+    contaDestino: '',
+    valor: 0,
+    dataTransferencia: ''
+  };
 
+  transferencias: Transferencia[] = [];
+
+  constructor(private service: TransferenciasService) {}
+
+  ngOnInit() {
+    this.listar();
+  }
+
+  listar() {
+    this.service.listar().subscribe((dados) => {
+      this.transferencias = dados;
+    });
+  }
+
+  agendar() {
+    this.service.agendar(this.novaTransferencia).subscribe({
+      next: (t) => {
+        this.transferencias.push(t);
+        this.novaTransferencia = {
+          contaOrigem: '',
+          contaDestino: '',
+          valor: 0,
+          dataTransferencia: ''
+        };
+      },
+      error: (err) => alert('Erro ao agendar transferência: ' + err.message)
+    });
+  }
 }
--- src/app/services/transferencias.service.ts
+++ src/app/services/transferencias.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Transferencia } from '../models/transferencia';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransferenciasService {
+  private api = 'http://localhost:8080/transferencias';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  listar(): Observable<Transferencia[]> {
+    return this.http.get<Transferencia[]>(this.api);
+  }
+
+  agendar(t: Transferencia): Observable<Transferencia> {
+    return this.http.post<Transferencia>(this.api, t);
+  }
 }
